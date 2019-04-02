f4a0e2e2bf92b38c255856b5b737580232aaaeea
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-04-02 15:21:58 +0900

    Stop Watch 2019 - 04 - 02

--- Stop Watch03/ViewController.swift
+++ Stop Watch03/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    var counter = 0
+    var myTimer = Timer()
+    var timer_running = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timeLabel.text = String(counter)
     }
 
-
+    @IBAction func timeStart(_ sender: Any) {
+        if !timer_running {
+            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            timer_running = true
+        }
+    }
+    
+    @IBAction func timePause(_ sender: Any) {
+        myTimer.invalidate()
+        counter = 0
+        timeLabel.text = String(counter)
+    }
+    @IBAction func timeStop(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    
+    @objc func updateTime() {
+        counter+=1
+        timeLabel.text = String(counter)
+    }
 }
 
